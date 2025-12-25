4d33de19a8660c013b5f41179c81f8b9dc3cd261
Jornole <jo@let-net.net>
2025-12-25 21:00:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 # -------------------------------------------------------------
 # VERSION
 # -------------------------------------------------------------
-version = "v1.1"
+version = "v1.11"
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
 
 st.markdown(
